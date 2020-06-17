5bc9b57737531193f153fab640cf3746489ccf20
Introversionya <55593273+Introversionya@users.noreply.github.com>
2020-06-18 02:46:04 +0500

    Настроил овл карусель в шапке

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,20 @@
-$(document).ready(function(){
-
+$(document).ready(function() {
+            
             const headerSlider = $(".owl-carousel");
+
+            headerSlider.on('initialize.owl.carousel initialized.owl.carousel', function(event) {
+                $('.slide-controls-number__active').text(event.item.index + 1)
+                $('.slide-controls-number__total').text(event.item.count)
+            });
+
+            
             headerSlider.owlCarousel({
             items: 1,
-            loop: true,
+            // loop: true,
             dots: false,
             smartSpeed: 1000
         });
-        
+
 
         $('#headerSliderLeft').click(function() {
             headerSlider.trigger('prev.owl.carousel');
@@ -17,4 +24,12 @@ $(document).ready(function(){
         $('#headerSliderRight').click(function() {
             headerSlider.trigger('next.owl.carousel');
         })
+
+
+        headerSlider.on('changed.owl.carousel', function(event) {
+            $('.slide-controls-number__active').text(event.item.index + 1)
+            $('.slide-controls-number__total').text(event.item.count)
+        });
+
+        
 });
